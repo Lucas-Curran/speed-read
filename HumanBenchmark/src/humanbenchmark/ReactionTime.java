ddc17f45ae9494214bccc18f04e0c6b560843e6f
Wersdgs <682935@sylvaniastudent.org>
2021-03-20 14:35:46 -0400

    Update ReactionTime.java

--- HumanBenchmark/src/humanbenchmark/ReactionTime.java
+++ HumanBenchmark/src/humanbenchmark/ReactionTime.java
@@ -20,23 +20,35 @@ public class ReactionTime extends Canvas implements MouseListener {
     private int counter = 0;
     private long begin, change;
     Random rNum = new Random();
-    private String text;
+    private String text1, text2, text3, changeText;
+    Color textColor;
     
     
     public ReactionTime() {
         addMouseListener(this);
 
-        this.setBackground(Color.red);
+        
  
       
-        text = "This is a reaction test. \nWhen the background turns green, click the screen. \n"
-                + "Click the screen to begin.";
+        text1 = "This is a reaction test.";
+        text2 = "When the background turns green, click the screen.";
+        text3 = "Click the screen to begin.";
+        changeText = "";
+        textColor = Color.WHITE;
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Consolas", Font.PLAIN, 36));
-        g.drawString(text, 50, 100);
+        g.setColor(textColor);
+        g.setFont(new Font("Consolas", Font.PLAIN, 24));
+        
+        g.drawString(changeText, this.getWidth()/2 - 50, this.getHeight()/2);
+        g.drawString(text1, 350, 100);
+        if (counter == 0) {
+            
+            g.drawString(text2, 190, 150);
+            g.drawString(text3, 350, 200);
+            this.setBackground(Color.red);
+        }
     }
 
 
@@ -49,10 +61,10 @@ public class ReactionTime extends Canvas implements MouseListener {
                 try {
                 System.out.println(counter);
                 
-                text = "";
+                text1 = "";
                 repaint();
                 Thread.sleep(rNum.nextInt(10000) + 1000);
-                this.setBackground(Color.green);
+                this.setBackground(Color.green.darker());
                 begin = System.currentTimeMillis();
                 } catch (InterruptedException ex) {
                     System.out.println(ex.getMessage());
@@ -64,9 +76,17 @@ public class ReactionTime extends Canvas implements MouseListener {
                 System.out.println("2");
                 change = e.getWhen() - begin;
                 System.out.println(change);
+                textColor = Color.BLACK;
+                changeText = String.valueOf(change) + " ms";
+                text1 = "Click the screen again to replay";
+                repaint();
                 break;
-            
             case 2:
+                textColor = Color.WHITE;
+                counter = -1;
+                changeText = "";
+                change = 0;
+                repaint();
                 break;
         
         }
