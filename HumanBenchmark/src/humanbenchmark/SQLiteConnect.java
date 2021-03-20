767bac1afb2fa2793a7bae5978fdff802adae2e0
Wersdgs <682935@sylvaniastudent.org>
2021-03-20 16:45:05 -0400

    .

--- HumanBenchmark/src/humanbenchmark/SQLiteConnect.java
+++ HumanBenchmark/src/humanbenchmark/SQLiteConnect.java
@@ -36,50 +36,20 @@ public class SQLiteConnect {
             return conn;
 	}
 	
-        
-	/**
-	 * Updates a student's schedule
-	 * @param info
-         * @param name
-	 */
-	public void updateReading(String name, ArrayList<String> info) {
-            String sql = "UPDATE Schedules SET"
-			+ " Subject1 = ? ,"
-			+ " Subject2 = ? ,"
-			+ " Subject3 = ? ,"
-			+ " Subject4 = ? ,"
-			+ " Subject5 = ? ,"
-			+ " Subject6 = ? ,"
-                        + " Subject7 = ? ,"
-                        + " Subject8 = ? ,"
-                        + " Subject9 = ?  "
-                        + " WHERE Name = ?";
-            try {
-		PreparedStatement input = connect("StudentSchedule.db").prepareStatement(sql);
-                input.setString(1,name);
-                for (int i = 1; i < info.size(); i++) {
-                    input.setString(i+1, info.get(i));
-                }
-		input.executeUpdate();
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-	}
+
 	/**
 	 * Creates a new student from the database
-	 * @param info
-         * @param name
+	 * @param timing
+         * @param reading
+         * @param date
 	 */
-	public void addTest(String name, ArrayList<String> info) {
-            String sql = "INSERT INTO Schedules(Name, Subject1, Subject2,"
-			+ " Subject3, Subject4, Subject5, Subject6, Subject7,"
-                        + " Subject8, Subject9) VALUES(?,?,?,?,?,?,?,?,?,?)";
+	public void addTest(int timing, int reading, String date) {
+            String sql = "INSERT INTO Data(ID, Timing, Reaction, Date) VALUES(?,?,?,?) ";
             try {
-		PreparedStatement input = connect("StudentSchedule.db").prepareStatement(sql);
-                for (int i = 0; i < info.size(); i++) {
-                    input.setString(i+1, info.get(i));
-                }
+		PreparedStatement input = connect("benchmark_data").prepareStatement(sql);
+                input.setInt(2, timing);
+                input.setInt(3, reading);
+                input.setString(4, date);
 		input.executeUpdate();
 		conn.close();
             } catch (SQLException e) {
@@ -94,11 +64,11 @@ public class SQLiteConnect {
          * @return
 	 */
 	public DefaultTableModel getData() {
-            String sql = "SELECT ID, Reading, Reaction, Date";
+            String sql = "SELECT * ";
             
             DefaultTableModel model = new DefaultTableModel();
             model.addColumn("ID");
-            model.addColumn("Reading");
+            model.addColumn("Timing");
             model.addColumn("Reaction");
             model.addColumn("Date");
             try {
